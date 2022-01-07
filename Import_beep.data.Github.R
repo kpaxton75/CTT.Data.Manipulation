9b90028a2738b4c6977f5b007b0b28ac4ff2a936
Kristina Paxton <72826644+kpaxton75@users.noreply.github.com>
2022-01-07 09:54:19 -1000

    Update Import_beep.data.Github.R
    updated annotation for a few sections to better explain the code

--- Import_beep.data.Github.R
+++ Import_beep.data.Github.R
@@ -8,18 +8,20 @@
 ##
 ##
 ##    Files Needed
-##        1. .csv file with a list of all nodes in your network and their latitude and longitude
+##        1. Nodes.csv file with a list of all nodes in your network that is saved in the working directory defined below
 ##            - Column names needed: NodeId
 ##            - Other columns can also be in the file for your reference
 ##            - Ensure all letters in NodeId are capitalized 
 ##            - If Node names are being converted to scientific notation in Excel open the file with a text editor (e.g. BBedit) to change names to the correct format and save the file
 ##
-##        2. .csv file with a list of all active tags for the specified time period
+##        2. Tags.csv file with a list of all active tags for the specified time period that is saved in the working directory defined below
 ##            -- Column names needed: TagId
 ##            -- other columns can also be in the file for your reference
 ##            -- Ensure all letters in TagId are capitalized
 ##
-##        3. Raw Beep data files
+##        3. Functions_CTT.Network.R file that contains function to run the script below - saved in the working directory defined below
+##
+##        4. Raw Beep data files
 ##            - When you download data from CTT using API - all csv files of raw beep data will be in a folder for your project and within that folder there will be folders for each Base Station 
 ##             - and within each Base Station folder there will be a folder named 'raw' that has raw beep data
 ##                   Ex. "/Users/kpaxton/DataFiles_CTT/Guam Sali/8EEEF7F20F8E/raw/CTT-8EEEF7F20F8E-raw-data.2020-08-26_111951.csv"
@@ -29,7 +31,7 @@
 ##                         -- 'CTT-8EEEF7F20F8E-raw-data.2020-08-26_111951.csv' is an example file name within the raw folder
 ##                         --  Everything prior to Project Name is the path on the computer where the files are found
 ##            - Verify that the date of the raw data file starts at the 44th character and ends at the 53rd character when counting from the Sensor Station name in the path
-##              ***** if this is not true for your data then you will need to change the numbers in Functions_CTT.Network.R line 26 to match your data **************
+##              ***** if this is not true for your data then you will need to change the numbers in Functions_CTT.Network.R line 27 to match your data **************
 ##     
 ##
 ##    Output
@@ -78,13 +80,19 @@ str(nodes) # check that data imported properly
         ## NODE.VERSION = Version of CTT node (needed for some CTT work flows)
         ## RADIOID = vector of RadioId to include - all values not included in the vector will be removed (e.g. radioid <- c(1,2,3) will include RadioId equal to 1,2, or 3)
         ## TIMEZONE = Time zone where data was collected, use grep("<insert location name here>", OlsonNames(), value=TRUE) to find valid time zone name
-        ## START and END = Start and end dates of interest based on UTC time - Guam (+10 UTC)
+        ## START and END = Start and end dates of interest based on UTC time (e.g., Guam (+10 UTC))
 
     ## Output in R environment
-        # beep.output - list containing beep_data (data frame with all beep data meeting specified time period and TagIds), count.date (number of rows removed that did not fall within specified time period),
-                        # beep.bad.dates (data frame with beep data with bad dates, e.g. 1970), count.import (number of rows imported), count.NA (number of rows removed that had NA values - e.g. BaseStations have NA for NodeId),
-                        # count.nodes (number of rows removed where the NodeId did not match the NodeId in the provided lookup table), count.duplicates (number of rows removed because of duplicate data, e.g. Base Stations with the same data),
-                        # count.ghosts (number of rows removed where the TagId did not match a TagId in the provided lookup table), count.RadioId (number of rows removed that did not match the specified RadioId)
+        # beep.output - list containing:
+                        # beep_data (data frame with all beep data meeting specified time period and TagIds)
+                        # count.date (number of rows removed that did not fall within specified time period)
+                        # beep.bad.dates (data frame with beep data with bad dates, e.g. 1970)
+                        # count.import (number of rows imported)
+                        # count.NA (number of rows removed that had NA values - e.g. BaseStations have NA for NodeId)
+                        # count.nodes (number of rows removed where the NodeId did not match the NodeId in the provided lookup table)
+                        # count.duplicates (number of rows removed because of duplicate data, e.g. Base Stations with the same data),
+                        # count.ghosts (number of rows removed where the TagId did not match a TagId in the provided lookup table)
+                        # count.RadioId (number of rows removed that did not match the specified RadioId)
 
   ## Output saved
       # .rds file of the beep_data save in the specified outpath
@@ -92,9 +100,9 @@ str(nodes) # check that data imported properly
 
 
 # Variables to define for function - replace values below with user specified values -
-INFILE <- "/Users/kpaxton/Dropbox (Personal)/HIresearch/9_HCSU/Micronesian.Starling.Movement/Guam_ARTS/DataFiles_CTT/Guam Sali"
+INFILE <- "/Users/kpaxton/DataFiles_CTT/Guam Sali"
 NODE.VERSION <- 2
-RADIOID <- c(1)
+RADIOID <- c(1,2)
 TIMEZONE <- "Pacific/Guam" 
 START <- "2021-05-18"
 END <- "2021-06-02"
@@ -109,10 +117,4 @@ beep_data <- beep.output[[1]]
 
 # Examine beep data that had bad dates (e.g., 1970, 2024, etc.) to see help diagnose potential nodes that are malfunctioning
 beep_bad <- beep.output[[2]]
-
-
-
-
-
-
 
